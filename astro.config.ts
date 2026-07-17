91c36a2a60ebdb3858550aeda525f870c15d211b
Thiago Trannin <51510921+thinety@users.noreply.github.com>
2026-07-17 12:18:22 +0200

    Set `site` to the correct URL in Astro config file

--- astro.config.ts
+++ astro.config.ts
@@ -2,6 +2,12 @@ import { defineConfig, fontProviders } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  // instead of adding `"types": ["node"]` to `tsconfig.json`, which would
+  // (wrongly) make types for node globals available everywhere in the project,
+  // we ignore this single usage of the `process` global, because we know
+  // this file is executed by Astro in a node process.
+  // @ts-expect-error
+  site: process.env.ASTRO_SITE,
   trailingSlash: "always", // match GitHub Pages setting
   fonts: [
     {
